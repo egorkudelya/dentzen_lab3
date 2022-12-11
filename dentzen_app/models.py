41de9974ae9dbb683c8503e3424bc2fa3900e40d
Kenobichek <lesechklesia@gmail.com>
2022-12-11 19:54:14 +0200

    Add models

--- dentzen_app/models.py
+++ dentzen_app/models.py
@@ -1,4 +1,11 @@
+from django.utils.timezone import now
 from django.db import models
+
+class Dentist(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.TextField(max_length=255, blank=True, null=True)
+    specialty = models.TextField(max_length=255, blank=True, null=True)
+    age = models.IntegerField(default=0)
 
 
 class DentalClinic(models.Model):
@@ -8,16 +15,12 @@ class DentalClinic(models.Model):
     location = models.TextField(max_length=255, blank=True, null=True)
     opens = models.TimeField(default='8:00:00')
     closes = models.TimeField(default='20:00:00')
-
-
-class Dentist(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.TextField(max_length=255, blank=True, null=True)
-    specialty = models.TextField(max_length=255, blank=True, null=True)
-    age = models.IntegerField(default=0)
+    dentists = models.ManyToManyField(Dentist, through = "DentistClinicContract")
 
 
 class DentistClinicContract(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField(default=now)
     clinic = models.ForeignKey(DentalClinic, on_delete=models.CASCADE)
     dentist = models.ForeignKey(Dentist, on_delete=models.CASCADE)
 
@@ -38,27 +41,35 @@ class Drug(models.Model):
     name = models.CharField(max_length=100)
     expiration_date = models.DateField(blank=True)
     description = models.TextField(max_length=255, blank=True, null=True)
-    supplier = models.ManyToManyField(Supplier)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
 
 
 class Instrument(models.Model):
     name = models.CharField(max_length=100)
     expiration_date = models.DateField(blank=True)
     description = models.TextField(max_length=255, blank=True, null=True)
-    supplier = models.ManyToManyField(Supplier)
-
-
-class Appointment(models.Model):
-    pass
-
-
-class Patient(models.Model):
-    pass
-
-
-class Diagnosis(models.Model):
-    pass
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
 
 
 class Disease(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    age_group = models.CharField(max_length=100)
+    description = models.TextField(max_length=255, blank=True, null=True)
+
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+
+
+class Diagnosis(models.Model):
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.TextField(max_length=255, blank=True, null=True)
+    date_time = models.DateTimeField(blank=True, default=now)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    dentist = models.ForeignKey(Dentist, on_delete=models.CASCADE)
